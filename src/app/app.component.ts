5776c71fc27bdbb821c451e09aca09a4bdeb0745
Cikle <cyrillutziger@gmail.com>
2025-03-28 15:07:11 +0100

    maybe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,30 @@ import { InventoryComponent } from './components/inventory/inventory.component';
 import { TradeBoxComponent } from './components/trade-box/trade-box.component';
 import { RouterOutlet, Router } from '@angular/router';
 import { SnipingComponent } from './sniping/sniping.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [InventoryComponent, TradeBoxComponent, RouterOutlet]
+  imports: [CommonModule, InventoryComponent, TradeBoxComponent, RouterOutlet, SnipingComponent]
 })
 export class AppComponent {
   title = 'equity';
   tradeItems: { name: string; icon: string }[] = [];
+  currentPage = '';
 
   constructor(private router: Router) { }
 
   goToPage(page: string): void {
     console.log(`Navigating to ${page}`);
-    this.router.navigate([`/${page.toLowerCase()}`]);
+    this.currentPage = page.toLowerCase();
+
+    if (page === '') {
+      this.router.navigate(['/']);
+    } else {
+      this.router.navigate([`/${page.toLowerCase()}`]);
+    }
   }
 }
