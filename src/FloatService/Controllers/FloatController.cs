d6138c8ff6a1dbae79c1b0a4e74c09460475df01
Marek von Rogall <marekvonrogall@gmx.ch>
2025-04-25 14:15:17 +0200

    Add endpoint to fetch skins from skinport.

--- src/FloatService/Controllers/FloatController.cs
+++ src/FloatService/Controllers/FloatController.cs
@@ -28,10 +28,39 @@ namespace FloatService.Controllers
             return Ok(data);
         }
 
+        [HttpGet("skinport")]
+        public async Task<IActionResult> GetSkinportItems()
+        {
+            var data = await FetchSkinportItems();
+            return Ok(data);
+        }
+
+        private async Task<JsonElement> FetchSkinportItems()
+        {
+            var url = "https://api.skinport.com/v1/items?app_id=730&currency=CHF&tradable=0";
+            
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Get, url))
+            {
+                requestMessage.Headers.Add("Accept-Encoding", "br");
+
+                HttpResponseMessage response = await _httpClient.SendAsync(requestMessage);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    return JsonDocument.Parse(content).RootElement;
+                }
+                else
+                {
+                    throw new HttpRequestException("Failed to fetch data from Skinport.");
+                }
+            }
+        }
+
         private async Task<JsonElement> FetchHighestDiscount()
         {
             string url = "https://csfloat.com/api/v1/listings?sort_by=highest_discount&type=buy_now";
-            HttpResponseMessage response = await httpClient2.GetAsync(url);
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
             if (response.IsSuccessStatusCode)
             {
                 string content = await response.Content.ReadAsStringAsync();
